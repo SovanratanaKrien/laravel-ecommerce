9ef5a500c74679078e7e9d6cc39604e8de42a296
Ratana <kriensovanratana@gmail.com>
2022-09-21 17:14:48 +0700

    FK U

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Made by SomeOne who Made It</div>
+
+        {{-- Footer menu --}}
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
